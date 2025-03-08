a5d4568c40d40ccc19c55b14071e9cec3b662256
diogotito <diogotitomarques@gmail.com>
2025-03-08 23:48:36 +0000

    What is viewPosition?

--- shaders/such_light_SimplePS.glsl
+++ shaders/such_light_SimplePS.glsl
@@ -1,13 +1,13 @@
 #version 330
 
 in vec3 objPosition;
+in vec3 viewPosition;
 in vec3 objNormal;
 in vec2 objUV;
-in mat3 tbn;
 
 out vec4 outColor;
 
-uniform vec3 viewPosition;
+uniform vec3 cameraPosition;
 uniform vec3 matColor;
 uniform float matShininess;
 uniform vec3 lightPos;
@@ -27,28 +27,74 @@ float posterize(float v) { return posterize(v, texRes); }
 void main() {
 	vec3 pos = objPosition;
 	
-	vec3 nView = normalize(viewPosition - pos);
-	vec3 nLight = normalize(lightPos - pos);
-	vec3 nNormal = normalize(objNormal);
-	vec3 nRefl = reflect(-nLight, nNormal);
+	// 1.)
+	vec2 originalUV = objUV;
+	vec2 centerUV = posterize(originalUV) + (0.5 / texRes);
+	vec2 dUV = centerUV - originalUV;
 	
-	float dotLN = dot(nLight, nNormal); // Facing ratio
-	//dotLN = posterize(dotLN, 4);
-	vec3 diffuse = lightDiffuse * dotLN;
-	vec3 specular = pow(max(0.0, dot(nView,nRefl)), matShininess) * lightSpecular;
+	// 2a.)
+	vec3 originalWorldPos = objPosition;
 	
-	float dst = distance(lightPos, pos);
-	float attn = 1./(1.0f + 0.1f*dst + 0.01f*dst*dst);
+	// 2b.)
+	vec2 dUVdS = dFdx(originalUV);
+	vec2 dUVdT = dFdy(originalUV);
+	
+	// 2c.)
+	mat2 dSTdUV = mat2(dUVdT[1], -dUVdT[0], -dUVdS[1], dUVdS[0])*(1.0f/(dUVdS[0]*dUVdT[1]-dUVdT[0]*dUVdS[1]));
+	
+	// 2d.)
+	vec2 dST = dSTdUV * dUV;
+	
+	// 2e.)
+	vec3 dXYZdS = dFdx(originalWorldPos);
+	vec3 dXYZdT = dFdy(originalWorldPos);
+	
+	// 2f.)
+	vec3 dXYZ = dXYZdS * dST[0] + dXYZdT * dST[1];
+	dXYZ = clamp(dXYZ, -1, 1);
+	
+	// 3.)
+	vec3 snappedWorldPos = originalWorldPos + dXYZ;
+	// UNITY_APPLY_DITHER_CROSSFADE(i.pos.xy);
+	
+	// 4.)
+	
+	
+	// -----------------------------------------------
+	
+	vec3 nView  = normalize(cameraPosition - pos);
+	vec3 nView2 = normalize(cameraPosition - snappedWorldPos);
+	vec3 nLight  = normalize(lightPos - pos);
+	vec3 nLight2 = normalize(lightPos - snappedWorldPos);
+	vec3 nNormal  = normalize(objNormal);
+	vec3 xTangent = dFdx(viewPosition);
+	vec3 yTangent = dFdy(viewPosition);
+	vec3 nNormal2 = normalize(cross( xTangent, yTangent ));
+	vec3 nRefl  = reflect(-nLight, nNormal);
+	vec3 nRefl2 = reflect(-nLight2, nNormal2);
+	
+	float dotLN  = dot(nLight, nNormal);   // Facing ratio
+	float dotLN2 = dot(nLight2, nNormal2); // Facing ratio, snapped edition
+	vec3 diffuse  = lightDiffuse * dotLN;
+	vec3 diffuse2 = lightDiffuse * dotLN2;
+	vec3 specular  = pow(max(0.0, dot(nView , nRefl )), matShininess) * lightSpecular;
+	vec3 specular2 = pow(max(0.0, dot(nView2, nRefl2)), matShininess) * lightSpecular;
+	
+	float dst  = distance(lightPos, pos);
+	float dst2 = distance(lightPos, snappedWorldPos);
+	float attn  = 1./(1.0f + 0.1f * dst  + 0.01f * dst  * dst );
+	float attn2 = 1./(1.0f + 0.1f * dst2 + 0.01f * dst2 * dst2);
 
-	vec3 color = ((diffuse + specular)*attn + lightAmbient)*matColor;
+	vec3 color  = ((diffuse  + specular ) * attn  + lightAmbient) * matColor;
+	vec3 color2 = ((diffuse2 + specular2) * attn2 + lightAmbient) * matColor;
 	
-	vec2 texelUV = posterize(objUV);
- 	
- 	// [DEBUG] Overlay texelUV
- 	color += vec3(abs(dFdx(texelUV)) + abs(dFdy(texelUV)), 0.0);
-	
-	// Posterize the color
-	// color = floor(color * texRes) / texRes;
+	#ifdef DEBUG
+ 	vec2 texelUV = posterize(objUV);
+ 	vec3 border = vec3(abs(dFdx(texelUV)) + abs(dFdy(texelUV)), 0.0);
+	float center = smoothstep(0.010, 0.005, length(dUV));
+	color += border; color2 += border; color += center; color2 += center;
+	#endif
 	
 	outColor = vec4(color, 1.0f);
+	outColor = vec4(color2, 1.0f);
 }
--- shaders/such_light_SimpleVS.glsl
+++ shaders/such_light_SimpleVS.glsl
@@ -1,5 +1,6 @@
 #version 330
 
+uniform mat4 matView;
 uniform mat4 matVP;
 uniform mat4 matGeo;
 
@@ -10,14 +11,14 @@ layout (location = 3) in vec3 bitangent;
 layout (location = 4) in vec2 texcoord;
 
 out vec3 objPosition;
+out vec3 viewPosition;
 out vec3 objNormal;
 out vec2 objUV;
-out mat3 tbn;
 
 void main() {
    objNormal = (matGeo * vec4(normal, 0)).xyz;
    objPosition = (matGeo * vec4(pos, 1)).xyz;
+   viewPosition = (matView * (matGeo * vec4(pos, 1))).xyz;
    objUV = texcoord;
-   tbn = mat3(tangent, bitangent, normal);
    gl_Position = matVP * matGeo * vec4(pos, 1);
 }
